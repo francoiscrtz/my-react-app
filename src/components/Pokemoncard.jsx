8ea5d725d648fd83ec00002f430e8829a7c2cfc9
francoiscrtz <francoiscreutzer@gmail.com>
2023-10-11 10:39:42 +0200

    dynamic

--- src/components/Pokemoncard.jsx
+++ src/components/Pokemoncard.jsx
@@ -1,12 +1,24 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function PokemonCard() {
-    return (
-      <figure>
-        <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-           />
-        <figcaption>Bulbasaur</figcaption>
-      </figure>
-    );
-  }
+  let pokemon = pokemonList[0];
+  return (
+    <figure>
+      {pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : <p>????</p>}
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>
+  );
+}
+
+export default PokemonCard;
   
-  export default PokemonCard;
+  
